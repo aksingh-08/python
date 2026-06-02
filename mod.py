6d66869e9159ee564e9efa0e6954a678442e219a
akscoder-08 <ashishk.singh1008@gmail.com>
2026-06-02 16:15:24 +0530

    module learning

---
+++ mod.py
@@ -0,0 +1,14 @@
+s = "If Comrade Napolean says it, it must be right."
+a = [100, 200, 300]
+def foo(arg):
+    print(f'arg = {arg}')
+
+class Foo:
+    pass 
+
+# import mod
+# mod.s
+# mod.a
+# mod.foo([1, 2, 3, 4])
+# x=mod.Foo()
+# 
