3f01d0cec41a0e6e411e02bca54f091a3353a5ae
akscoder-08 <ashishk.singh1008@gmail.com>
2026-06-02 23:44:37 +0530

    Initialized subpackages with program updation

--- pkg/__init__.py
+++ pkg/__init__.py
@@ -2,7 +2,9 @@ print(f'Invoking __init__.py for {__name__}')
 
 A = ['quux', 'corge', 'grault']
 
-import pkg.mod1, pkg.mod2
+# import pkg.mod1, pkg.mod2
+from pkg.sub_pkg1 import *
+from pkg.sub_pkg2 import *
 
 __all__ = [
     'mod1',
