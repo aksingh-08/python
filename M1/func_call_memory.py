538df6279a83395e616be2de5094db07b6334a00
akscoder-08 <ashishk.singh1008@gmail.com>
2026-06-06 17:39:06 +0530

    update

--- M1/func_call_memory.py
+++ M1/func_call_memory.py
@@ -1,3 +1,15 @@
+def demonstrate(st, num):
+    print(f"    id(st) inside: {id(st)}")
+    print(f"    id(num) inside: {id(num)}")
+my_list = [1, 2, 3]
+my_num = 42
+
+print(f"id(my_list): {id(my_list)}")
+print(f"id(my_num): {id(my_num)}")
+demonstrate(my_list, my_num)
+
+print("*****************************************************************")
+
 def mutate(lst):
     lst.append(99) # Mutates shared object - VISIBLE to caller
 
