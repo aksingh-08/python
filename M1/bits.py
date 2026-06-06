bf389f7320fa1ac91e57fb7a5a964231403f277c
akscoder-08 <ashishk.singh1008@gmail.com>
2026-06-07 00:20:33 +0530

    verifying arithmetic notation

---
+++ M1/bits.py
@@ -0,0 +1,16 @@
+print(bin(42))
+print(bin(255))
+print(bin(0))
+
+x = 0b101010
+print(x)
+print(type(x))
+
+print(int("101010", 2))
+print(int("11111111", 2))
+print(int("0b101010", 2))
+
+print(hex(42))
+print(oct(42))
+print(0xFF)
+print(0o52)
