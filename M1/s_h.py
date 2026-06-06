fbfec6b9f7c94bab9b892af673f99c907c5a285f
akscoder-08 <ashishk.singh1008@gmail.com>
2026-06-06 16:06:51 +0530

    Reassignment and Mutation

--- M1/s_h.py
+++ M1/s_h.py
@@ -1,13 +1,4 @@
-x=[1, 2, 3]
-y=x
-y.append(4)
-print(x)
-print(id(x))
-print(y)
-print(id(y))
-
-#
-
+# Reassignment - moves the name to a different object:
 a=5
 b=a
 b=6
@@ -15,3 +6,16 @@ print(a)
 print(id(a))
 print(b)
 print(id(b))
+print(a==b)
+
+
+# Mutaion - changes the object itself (all names see the change):
+x=[1, 2, 3]
+y=x
+y.append(4)
+print(x)
+print(id(x))
+print(y)
+print(id(y))
+print(x==y)
+
