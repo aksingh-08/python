d9691eae01dbfe2e403f0aa47e2dc0d3f9dfb61a
akscoder-08 <ashishk.singh1008@gmail.com>
2026-06-11 17:11:41 +0530

    ...

--- bug.py
+++ bug.py
@@ -4,12 +4,18 @@
 # print(add_item("apple"))
 # print(add_item("banana"))
 # print(add_item("cherry"))
-# 
-def add_item(item, collection=None):
-    if collection is None:
-        collection = []
-    collection.append(item)
-print(add_item("apple"))
-print(add_item("banana"))
 
 
+# def add_item(item, coection=None):
+    # if collection is None:
+        # collection = []
+    # collection.append(item)
+# print(add_item("apple"))
+# print(add_item("banana"))
+
+
+print(round(0.5))
+print(round(1.5))
+print(round(2.5))
+print(round(3.5))
+print(round(4.5))
